87da09029577be34d2159c4aeee59d087359d0db
Ruoshan Huang <ruoshan.huang@gmail.com>
2022-01-12 18:18:21 +0800

    Enable static build for both binaries

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 set -xe
 
-# Build a static agent binary
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build --ldflags "-s" ./cmd/apf-agent
+# Enable static build
+export CGO_ENABLED=0
+
+GOOS=linux GOARCH=amd64 go build --ldflags "-s" ./cmd/apf-agent
 
 if command -v upx; then
-    upx apf-agent
+    # upx apf-agent
+    echo a
 fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
     tar -c --uid 0 --uname root --gid 0 --gname root -f agent.tar apf-agent
@@ -14,6 +17,6 @@ else
     tar -c --group=root:0 --owner=root:0 -f agent.tar apf-agent
 fi
 mv agent.tar bootstrap
-rm -f apf-agent
+# rm -f apf-agent
 
 go build -ldflags="-X main.version=${VERSION:-dev}" ./cmd/apf
